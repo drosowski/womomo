39ed9c63876c09439c689f38b2aa923b740055da
Daniel Rosowski <daniel.rosowski@gmx.de>
2011-08-08 21:52:49 +0200

    Setup greenmail (had to copy, since overriding view didnt work)

---
+++ grails-app/controllers/de/womomo/GreenmailController.groovy
@@ -0,0 +1,58 @@
+package de.womomo
+
+import com.icegreen.greenmail.util.GreenMailUtil
+import grails.converters.JSON
+
+class GreenmailController {
+
+    def greenMail
+
+    def index = {redirect action: 'list'}
+
+    def list = {
+        withFormat {
+            js {
+                List jsonMessages = []
+                List messages = greenMail.getReceivedMessages().sort({it.sentDate}).reverse()
+                messages.eachWithIndex {message, index ->
+                    jsonMessages << createMessageMap(message, index)
+                }
+                render jsonMessages as JSON
+            }
+            html {
+                return [list: greenMail.getReceivedMessages().sort({it.sentDate}).reverse()]
+            }
+        }
+    }
+
+    def show = {
+        def messages = Arrays.asList(greenMail.getReceivedMessages().sort({it.sentDate}).reverse())
+        def specificMessage = messages[Integer.valueOf(params.id).intValue()]
+        withFormat {
+            js {
+                render createMessageMap(specificMessage, params.id) as JSON
+            }
+            html {
+                render "<pre>${GreenMailUtil.getWholeMessage(specificMessage)}</pre>"
+            }
+        }
+    }
+
+    private createMessageMap(message, index) {
+        Map messageMap = [
+                id: index,
+                sent: message.sentDate,
+                subject: message.subject,
+                to: GreenMailUtil.getAddressList(message.getRecipients(javax.mail.Message.RecipientType.TO)),
+                body: GreenMailUtil.getWholeMessage(message)
+        ]
+        return messageMap
+    }
+
+    def clear = {
+        greenMail.stop()
+        greenMail.start()
+        render 'Email messages have been cleared'
+    }
+
+}
